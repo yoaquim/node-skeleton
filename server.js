cb06b6610c22a87b57b6c2d8a4601f20eb8629a1
Yoaquim Cintrón <yoaquim@outlook.com>
2016-03-04 18:45:47 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,51 @@
+// Dependencies
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+// Express app setup
+var app = express();
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Error handlers
+// -------------
+
+// Catch 404 error and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Development (stacktraces on)
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.send({
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// Production (stacktraces on)
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.send({
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
